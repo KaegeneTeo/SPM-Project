f765152cd75e90c01b223f93dfed3cb9d6773d94
KaegeneTeo <Kaegenie@gmail.com>
2024-09-13 18:07:47 +0800

    working layout UI

---
+++ aiowa/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <MainLayout title="Dashboard">
+    <!-- Your main content goes here -->
+    <router-view></router-view> <!-- This will render the component corresponding to the current route -->
+  </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from './components/MainLayout.vue'
+</script>
