1af2632c5a046157f495877dcd9f1dd883b36b6c
Ll0m0 <zhangbowen9431@gmail.com>
2024-09-19 18:41:53 +0800

    Updated Schedule page with the following features: 1. display events color coded by AM & PM 2. display events count on month and year view 3. display card with list of employees taking WFH when clicking on wfh slot This is only tested to work using dummy data, but theoretically would work if data passed in is of the right format

--- aiowa/src/main.js
+++ aiowa/src/main.js
@@ -2,7 +2,16 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
-import vuetify from './plugins/vuetify';
+import { createVuetify } from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 createApp(App)
   .use(vuetify)
