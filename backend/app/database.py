3c8053a49b4bae2162a6454207c2e52e503e3936
KaegeneTeo <Kaegenie@gmail.com>
2024-09-24 14:35:44 +0800

    error message for failed login, rds tested

--- backend/app/database.py
+++ backend/app/database.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 
+# dbURL = 'mysql+mysqlconnector://admin:#ZD6mi7nvT#BBK@spmdb.c5okg8oqe41e.us-east-1.rds.amazonaws.com:3306/HRMS'  #rdslink
 dbURL = 'mysql+mysqlconnector://root@localhost:3306/hrms'
 #dbURL = 'mysql+mysqlconnector://admin:#ZD6mi7nvT#BBK@spmdb.c5okg8oqe41e.us-east-1.rds.amazonaws.com:3306/HRMS'
 #dbURL = 'mysql+mysqlconnector://root:root@localhost:8889/hrms'
