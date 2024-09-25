440c36abd5bbed757b31bad8a81c48207cae0b2a
Gerard Loh <134910322+gerardloh@users.noreply.github.com>
2024-09-25 18:55:27 +0800

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -8,9 +8,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 
 # dbURL = 'mysql+mysqlconnector://admin:#ZD6mi7nvT#BBK@spmdb.c5okg8oqe41e.us-east-1.rds.amazonaws.com:3306/HRMS'  #rdslink
-#dbURL = 'mysql+mysqlconnector://root@localhost:3306/hrms'
+dbURL = 'mysql+mysqlconnector://root@localhost:3306/hrms'
 #dbURL = 'mysql+mysqlconnector://admin:#ZD6mi7nvT#BBK@spmdb.c5okg8oqe41e.us-east-1.rds.amazonaws.com:3306/HRMS'
-dbURL = 'mysql+mysqlconnector://root:root@localhost:8889/hrms'
+#dbURL = 'mysql+mysqlconnector://root:root@localhost:8889/hrms'
 engine = create_engine(dbURL, echo=True)  #changed back to DATABASE_URL if needed
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
