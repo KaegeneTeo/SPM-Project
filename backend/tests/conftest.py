d8f0fbeb7b647a3dd8b6286587f685d844ac5f4b
Ll0m0 <88282240+Ll0m0@users.noreply.github.com>
2024-10-14 00:30:24 +0800

    using alternative method to create client in hopes of solving 404 error from existing endpoint

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,30 +1,9 @@
 import pytest
-from app import create_app
+from app import app
 import os
 from supabase import create_client, Client
 
 @pytest.fixture()
-def app():
-    app = create_app()
-    app.config.update({
-        "TESTING": True,
-    })
-
-    # other setup can go here
-    url: str = os.getenv("SUPABASE_URL")
-    key: str = os.getenv("SUPABASE_KEY")
-    supabase: Client = create_client(url, key)
-
-    yield app
-
-    # clean up / reset resources here
-
-
-@pytest.fixture()
-def client(app):
-    return app.test_client()
-
-
-@pytest.fixture()
-def runner(app):
-    return app.test_cli_runner()
+def client():
+    with app.test_client() as client:   
+        yield client
