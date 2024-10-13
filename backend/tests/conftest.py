cef616d51b6f00c9e0da2496f7ee817c09ffcc49
Ll0m0 <88282240+Ll0m0@users.noreply.github.com>
2024-10-14 00:36:06 +0800

    switched back to create_app

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,9 +1,30 @@
 import pytest
-from app import app
+from app import create_app
 import os
 from supabase import create_client, Client
 
 @pytest.fixture()
-def client():
-    with app.test_client() as client:   
-        yield client
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+    url: str = os.getenv("SUPABASE_URL")
+    key: str = os.getenv("SUPABASE_KEY")
+    supabase: Client = create_client(url, key)
+
+    yield app
+
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
