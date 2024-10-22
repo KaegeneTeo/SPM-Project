74b756688a37e88796d8811c86ed14d6b85db3d5
Ll0m0 <88282240+Ll0m0@users.noreply.github.com>
2024-10-22 13:28:58 +0800

    added partial notif engine just to allow me to push bug fix

--- backend/flaskapp/models/notification.py
+++ backend/flaskapp/models/notification.py
@@ -6,14 +6,31 @@ class notification_engine:
     #compose email on accepted schedule
     def compose_on_accept(self, request_id):
         email = ""
-        request_response = self.supabase.from_("schedules").select("*").eq("request_id", request_id).execute()
+        request_response = self.supabase.from_("request").select("*").eq("request_id", request_id).execute()
 
         if not request_response.data:
-            return {"error": "Schedules not found"}, 404
+            return {"error": "Requests not found"}, 404
 
-        selected_request = request_response.data
-        schedulelist = list(selected_request)
-        dict
+        selected_request = request_response.data[0]
+
+        staffid = selected_request["staff_id"]
+
+        employee_response = self.supabase.from_("Employee").select("*").eq("Staff_ID", staffid).execute()
+
+        if not request_response.data:
+            return {"error": "Employee not found"}, 404
+        
+        employee = employee_response.data[0]
+        staffname = employee["Staff_FName"] + " " + employee["Staff_LName"]
+
+        email += f"Hi ${staffname}, your request (ID: ${selected_request["request_id"]}) from ${selected_request["startdate"]} to ${selected_request["enddate"]} has been partially or fully approved. Please check your schedule for details."
+
+class notification_sender:
+    def __init__(self, notif_engine):
+        self.notif_engine = notif_engine
+    
+    def send_to_employee():
+        return
 
         
 
